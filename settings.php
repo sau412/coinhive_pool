1bd25aabc9e09b223e02f42a042f8f808c40017f
Tsarev Vladimir <sau412@gmail.com>
2018-10-16 17:32:18 +0300

    Added

---
+++ settings.php
@@ -0,0 +1,22 @@
+<?php
+// Settings file
+
+// DB variables
+$db_host="";
+$db_login="";
+$db_password="";
+$db_base="";
+
+// Salt for password
+$salt="";
+
+// Cooldown limit
+$cooldown_limit=900;
+
+$coinhive_private_key="";
+$coinhive_public_key="";
+
+// Referral rate
+$hashes_ref_rate=0.01;
+$hashes_ref_rate_level_2=0.01;
+?>
