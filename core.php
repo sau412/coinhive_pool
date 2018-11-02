d64728d5adf3cd6d3ffb88976e574e2792d9dde9
Tsarev Vladimir <sau412@gmail.com>
2018-11-02 16:03:52 +0300

    New functions encapsulated

--- core.php
+++ core.php
@@ -37,8 +37,8 @@ function set_variable($name,$value) {
         db_query("INSERT INTO `variables` (`name`,`value`) VALUES ('$name_escaped','$value_escaped') ON DUPLICATE KEY UPDATE `value`=VALUES(`value`)");
 }
 
-// Get user balance
-function get_user_balance($user_uid) {
+// Get detailed balance
+function get_user_balance_detail($user_uid) {
         global $hashes_ref_rate;
         global $hashes_ref_rate_level_2;
 
@@ -48,11 +48,30 @@ function get_user_balance($user_uid) {
         $withdrawn=db_query_to_variable("SELECT `withdrawn` FROM `users` WHERE `uid`='$user_uid_escaped'");
         $ref1=db_query_to_variable("SELECT SUM(`mined`) FROM `users` WHERE `ref_id`='$user_uid_escaped'");
         $ref2=db_query_to_variable("SELECT SUM(`mined`) FROM `users` WHERE `ref_id` IN (SELECT `uid` FROM `users` WHERE `ref_id`='$user_uid_escaped')");
+
         $ref1=floor($ref1*$hashes_ref_rate);
         $ref2=floor($ref2*$hashes_ref_rate_level_2);
 
-        $hashes=$mined+$ref1+$ref2+$bonus-$withdrawn;
-        return $hashes;
+        $ref_total=$ref1+$ref2;
+
+        $balance=$mined+$ref1+$ref2+$bonus-$withdrawn;
+
+        return array(
+                "mined"=>$mined,
+                "bonus"=>$bonus,
+                "withdrawn"=>$withdrawn,
+                "ref_total"=>$ref_total,
+                "balance"=>$balance,
+                "ref1"=>$ref1,
+                "ref2"=>$ref2,
+        );
+}
+
+// Get user balance
+function get_user_balance($user_uid) {
+        $balance_detail=get_user_balance_detail($user_uid);
+
+        return $balance_detail['balance'];
 }
 
 // Get badges
@@ -108,6 +127,159 @@ function is_cooltime_active($user_uid) {
         $cooldown_time=db_query_to_variable("SELECT MIN(UNIX_TIMESTAMP(NOW())-UNIX_TIMESTAMP(COALESCE(`cooldown`,0))) FROM `users` WHERE `uid`='$user_uid_escaped'");
         if($cooldown_time<$cooldown_limit) return TRUE;
         else return FALSE;
+}
+
+// Get username by user_uid
+function get_username_by_uid($user_uid) {
+        $user_uid_escaped=db_escape($user_uid);
+        $username=db_query_to_variable("SELECT `username` FROM `users` WHERE `uid`='$user_uid_escaped'");
+        return $username;
+}
+
+// Update user balance
+function update_user_mined_balance($user_uid,$new_balance) {
+        $user_uid_escaped=db_escape($user_uid);
+        $new_balance_escaped=db_escape($new_balance);
+        db_query("UPDATE `users` SET `mined`='$new_balance_escaped' WHERE uid='$user_uid_escaped' AND `mined`<='$new_balance_escaped'");
+}
+
+function get_session() {
+        if(isset($_COOKIE['session_id']) && validate_ascii($_COOKIE['session_id'])) {
+                $session=$_COOKIE['session_id'];
+                $session_escaped=db_escape($session);
+                $session_exists=db_query_to_variable("SELECT 1 FROM `sessions` WHERE `session`='$session_escaped'");
+                if(!$session_exists) {
+                        unset($session);
+                }
+        }
+
+        if(!isset($session)) {
+                $session=bin2hex(random_bytes(32));
+                $token=bin2hex(random_bytes(32));
+                setcookie('session_id',$session,time()+86400*30);
+                $session_escaped=db_escape($session);
+                $token_escaped=db_escape($token);
+                db_query("INSERT INTO `sessions` (`session`,`token`) VALUES ('$session_escaped','$token_escaped')");
+        }
+        return $session;
+}
+
+function get_user_uid_by_session($session) {
+        $session_escaped=db_escape($session);
+        $user_uid=db_query_to_variable("SELECT `user_uid` FROM `sessions` WHERE `session`='$session_escaped'");
+        return $user_uid;
+}
+
+function get_user_token_by_session($session) {
+        $session_escaped=db_escape($session);
+        $token=db_query_to_variable("SELECT `token` FROM `sessions` WHERE `session`='$session_escaped'");
+        return $token;
+}
+
+function user_register_or_login($session,$login,$password,$ref_id) {
+        global $salt;
+
+        $session_escaped=db_escape($session);
+        $password_hash=hash("sha256",$password.strtolower($login).$salt);
+
+        $message="";
+
+        if(validate_ascii($login)) {
+                $login_escaped=db_escape($login);
+                $ref_id_escaped=db_escape($ref_id);
+                $exists_hash=db_query_to_variable("SELECT `password_hash` FROM `users` WHERE `username`='$login_escaped'");
+                if($exists_hash=="") {
+                        write_log("New user '$login' ref '$ref_id'");
+                        $login_escaped=db_escape($login);
+                        db_query("INSERT INTO `users` (`username`,`password_hash`,`ref_id`) VALUES ('$login_escaped','$password_hash','$ref_id_escaped')");
+                        $user_uid=db_query_to_variable("SELECT `uid` FROM `users` WHERE `username`='$login_escaped'");
+                        db_query("UPDATE `sessions` SET `user_uid`='$user_uid' WHERE `session`='$session_escaped'");
+                } else if($password_hash==$exists_hash) {
+                        write_log("Logged in user '$login'");
+                        $user_uid=db_query_to_variable("SELECT `uid` FROM `users` WHERE `username`='$login_escaped'");
+                        $user_uid_escaped=db_escape($user_uid);
+                        db_query("UPDATE `sessions` SET `user_uid`='$user_uid' WHERE `session`='$session_escaped'");
+                } else {
+                        write_log("Invalid password for '$login'");
+                        $message="Invalid password";
+                }
+        } else {
+                write_log("Invalid login for '$login'");
+                $message="Invalid login";
+        }
+        return $message;
+}
+
+function user_logout($session) {
+        $user_uid=get_user_uid_by_session($session);
+        $username=get_username_by_uid($user_uid);
+        write_log("Logged out user '$username'");
+
+        $session_escaped=db_escape($session);
+        db_query("UPDATE `sessions` SET `user_uid`=NULL WHERE `session`='$session_escaped'");
+}
+
+function user_withdraw($session,$user_uid,$currency_code,$payout_address,$payment_id) {
+        global $email_notify;
+
+        $message="";
+
+        $session_escaped=db_escape($session);
+        $user_uid_escaped=db_escape($user_uid);
+        $username=get_username_by_uid($user_uid);
+
+        $balance_info=get_user_balance_detail($user_uid);
+
+        $hashes_mined=$balance_info['mined'];
+        $hashes_withdrawn=$balance_info['withdrawn'];
+        $hashes_bonus=$balance_info['bonus'];
+        $hashes_ref=$balance_info['ref_total'];
+        $hashes_balance=$balance_info['balance'];
+
+        if(is_cooltime_active($user_uid)) {
+                $message="One withdraw in 15 minutes";
+        } else if($hashes_balance<=0) {
+                $message="Nothing to withdraw";
+        } else {
+                $currency_code_escaped=db_escape($currency_code);
+                $rate_per_mhash=db_query_to_variable("SELECT `rate_per_mhash` FROM `currency` WHERE `currency_code`='$currency_code_escaped'");
+                $payout_fee=db_query_to_variable("SELECT `payout_fee` FROM `currency` WHERE `currency_code`='$currency_code_escaped'");
+                $project_fee=db_query_to_variable("SELECT `project_fee` FROM `currency` WHERE `currency_code`='$currency_code_escaped'");
+
+                $amount=$hashes_balance*$rate_per_mhash/1000000;
+                $total=$amount-$payout_fee-$project_fee;
+                $amount=sprintf("%0.8f",$amount);
+                $amount_escaped=db_escape($amount);
+                $total_escaped=db_escape($total);
+                $total=sprintf("%0.8f",$total);
+
+                if($total>0) {
+                        $hashes_withdrawn_new=$hashes_withdrawn+$hashes_balance;
+                        db_query("UPDATE `users` SET `withdrawn`='$hashes_withdrawn_new',`cooldown`=NOW() WHERE `uid`='$user_uid_escaped'");
+
+                        write_log("Withdraw user '$username' amount '$hashes_balance' (from mined '$hashes_mined' ref '$hashes_ref' bonus '$hashes_bonus' withdrawn '$hashes_withdrawn') coin '$currency_code' total '$total' address '$payout_address'");
+
+                        email_add($email_notify,"Withdraw '$username' '$total' '$currency_code'",
+                                "Withdraw user '$username' amount '$hashes_balance' (from mined '$hashes_mined' ref '$hashes_ref' bonus '$hashes_bonus' withdrawn '$hashes_withdrawn')
+coin '$currency_code' total '$total' address '$payout_address'");
+
+                        $address_escaped=db_escape($payout_address);
+                        $payment_id_escaped=db_escape($payment_id);
+                        $rate_per_mhash_escaped=db_escape($rate_per_mhash);
+                        $payout_fee_escaped=db_escape($payout_fee);
+                        $project_fee_escaped=db_escape($project_fee);
+                        $hashes_escaped=db_escape($hashes_balance);
+
+                        db_query("INSERT INTO `payouts` (`session`,`user_uid`,`currency_code`,`address`,`payment_id`,`hashes`,`rate_per_mhash`,`amount`,`payout_fee`,`project_fee`,`total`)
+VALUES ('$session_escaped','$user_uid_escaped','$currency_code_escaped','$address_escaped','$payment_id_escaped','$hashes_escaped','$rate_per_mhash_escaped','$amount_escaped','$payout_fee_escaped','$project_fee_escaped','$total_escaped')");
+
+                        $message="Request sent";
+                } else {
+                        $message="Payout limit not reached";
+                }
+        }
+
+        return $message;
 }
 
 // For php 5 only variant for random_bytes is openssl_random_pseudo_bytes from openssl lib
