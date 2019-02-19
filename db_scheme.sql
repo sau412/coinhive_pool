9b0c2f2546eb98abaf4e9e7816b5f819d39f5ced
Tsarev Vladimir <sau412@gmail.com>
2019-02-19 21:34:23 +0300

    Asset support

--- db_scheme.sql
+++ db_scheme.sql
@@ -1,6 +1,16 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+CREATE TABLE IF NOT EXISTS `assets` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `user_uid` int(11) NOT NULL,
+  `currency` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `balance` double NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `user_uid` (`user_uid`,`currency`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 CREATE TABLE IF NOT EXISTS `currency` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
   `currency_code` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
@@ -14,6 +24,7 @@ CREATE TABLE IF NOT EXISTS `currency` (
   `img_url` text COLLATE utf8_unicode_ci NOT NULL,
   `payment_id_field` int(11) NOT NULL DEFAULT '0',
   `rate_per_mhash` double NOT NULL DEFAULT '0',
+  `btc_per_coin` double NOT NULL DEFAULT '0',
   `user_withdraw_note` text COLLATE utf8_unicode_ci NOT NULL,
   `admin_withdraw_note` text COLLATE utf8_unicode_ci NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -67,6 +78,31 @@ CREATE TABLE IF NOT EXISTS `payouts` (
   PRIMARY KEY (`uid`),
   KEY `status` (`status`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE IF NOT EXISTS `payout_stats` (
+`currency_code` varchar(100)
+,`sum_total` double
+,`sum_hashes` decimal(32,0)
+,`acount` bigint(21)
+,`distinct_users` bigint(21)
+);
+CREATE TABLE IF NOT EXISTS `platforms` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `currency` varchar(100) NOT NULL,
+  `rate` double NOT NULL,
+  `caption` varchar(100) NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `results` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `user_uid` int(11) NOT NULL,
+  `platform` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `value` double NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `user_uid` (`user_uid`,`platform`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `sessions` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
@@ -106,3 +142,6 @@ CREATE TABLE IF NOT EXISTS `variables` (
   PRIMARY KEY (`uid`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+DROP TABLE IF EXISTS `payout_stats`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`u0403486_hive`@`localhost` SQL SECURITY DEFINER VIEW `payout_stats` AS select `payouts`.`currency_code` AS `currency_code`,sum(`payouts`.`total`) AS `sum_total`,sum(`payouts`.`hashes`) AS `sum_hashes`,count(0) AS `acount`,count(distinct `payouts`.`user_uid`) AS `distinct_users` from `payouts` where (`payouts`.`tx_id` <> '') group by `payouts`.`currency_code`;
