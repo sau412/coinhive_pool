1ee5a3933a639ec720842c18db1a2a1ab12b7c65
Tsarev Vladimir <sau412@gmail.com>
2019-03-18 12:48:36 +0300

    Deposits options

--- settings.php
+++ settings.php
@@ -35,4 +35,20 @@ $email_notify="";
 // Referral rate
 $hashes_ref_rate=0.01;
 $hashes_ref_rate_level_2=0.01;
+
+// Deposit options
+$deposit_currencies=array("BTC"=>"Bitcoin","LTC"=>"Litecoin","GRC"=>"Gridcoin","SHND"=>"StrongHands");
+
+// Api keys and links
+$btc_api_url="https://wallet.arikado.ru/btc/api.php";
+$btc_api_key="";
+
+$ltc_api_url="https://wallet.arikado.ru/ltc/api.php";
+$ltc_api_key="";
+
+$grc_api_url="https://wallet.arikado.ru/grc/api.php";
+$grc_api_key="";
+
+$shnd_api_url="https://wallet.arikado.ru/shnd/api.php";
+$shnd_api_key="";
 ?>
